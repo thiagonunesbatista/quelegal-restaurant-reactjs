50509b2cbe7d89190ac8ba49f0973defd564feb9
eduardogsouz <eduardogoncsouza@gmail.com>
2023-11-28 20:33:15 -0300

    fix: correction of uploaded conflicts

--- src/Components/sections/RegisterMenu.tsx
+++ src/Components/sections/RegisterMenu.tsx
@@ -1,4 +1,4 @@
-import { useForm } from 'react-hook-form';
+import { useForm } from "react-hook-form";
 
 // import { Input } from '../Input';
 type types = {
@@ -9,22 +9,21 @@ export const RegisterMenu = ({ onAdd }: types) => {
   const { register, reset, handleSubmit } = useForm();
 
   const onSubmit = (data: any) => {
-    const currentSaved = localStorage.getItem('added-menu');
+    const currentSaved = localStorage.getItem("added-menu");
 
     if (currentSaved) {
       const transformed = JSON.parse(currentSaved);
 
       transformed.push(data);
-      localStorage.setItem('added-menu', JSON.stringify(transformed));
+      localStorage.setItem("added-menu", JSON.stringify(transformed));
     } else {
-      localStorage.setItem('added-menu', JSON.stringify([data]));
+      localStorage.setItem("added-menu", JSON.stringify([data]));
     }
     onAdd();
     reset();
   };
 
   return (
-<<<<<<< HEAD
     <div className="flex flex-col items-center justify-center mx-auto w-full max-w-[38rem] my-10 gap-4 shadow-2xl p-10 rounded-3xl">
       <p className="text-4xl mb-10">Cadastro de Prato</p>
 
@@ -44,7 +43,7 @@ export const RegisterMenu = ({ onAdd }: types) => {
         />
         <input
           className="border border-[#E0E0E0] p-3"
-          placeholder="Imagem  do Prato"
+          placeholder="Imagem do Prato"
           {...register("image")}
         />
         <textarea
@@ -56,36 +55,6 @@ export const RegisterMenu = ({ onAdd }: types) => {
         <button
           className="bg-[#FF9F0D] text-white p-2 text-lg rounded-2xl w-[32rem]"
           type="submit"
-=======
-    <div className='flex flex-col items-center mx-auto w-full max-w-[400px] my-10 gap-4 shadow-2xl p-10'>
-      <p>Cadastro de Prato</p>
-
-      <form className='flex flex-col gap-6' onSubmit={handleSubmit(onSubmit)}>
-        <input
-          className='border border-[#E0E0E0]'
-          placeholder='Nome do Prato'
-          {...register('title')}
-        />
-        <input
-          className='border border-[#E0E0E0]'
-          placeholder='Valor do Prato'
-          {...register('price')}
-        />
-        <input
-          className='border border-[#E0E0E0]'
-          placeholder='Imagem  do Prato'
-          {...register('image')}
-        />
-        <input
-          className='border border-[#E0E0E0]'
-          placeholder='Descrição do Prato'
-          {...register('description')}
-        />
-
-        <button
-          className='bg-[#FF9F0D] text-white p-2 text-lg rounded-2xl'
-          type='submit'
->>>>>>> 1bb5013cdc2a16079e8991f9a846f1cd7a5c2c0f
         >
           Cadastrar Prato
         </button>
